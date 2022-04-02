b2c3e301dcbdf0240d692067c9bc8abb03ed80a2
UTN-FRGP-2022-LabV <sergio.palacio@alumnos.frgp.utn.edu.ar>
2022-04-02 15:05:01 -0300

    Agrego clase constantes

---
+++ src/com/utn/eventos/constantes/Constantes.java
@@ -0,0 +1,12 @@
+package com.utn.eventos.constantes;
+
+public class Constantes {
+	public static final double COSTO_FIJO_VIP = 1500;
+	public static final double COSTO_FIJO_GENERAL = 800;
+	public static final double COSTO_FIJO_GENERAL_TEATRO = 1350.50;
+	public static final double COSTO_FIJO_INFANTIL_MENOR_8_ANIOS = 1350.50;
+	public static final double COSTO_FIJO_INFANTIL_MAYOR_8_ANIOS = 1350.50;
+	public static final double COSTO_FIJO_DEPORTE_FUTBOL = 300;
+	public static final double COSTO_FIJO_DEPORTE_RUGBY = 450;
+	public static final double COSTO_FIJO_DEPORTE_JOCKEY = 380;
+}
